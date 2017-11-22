8c52dcd5edb77e56ea79cd4d381c7fffaa4cca46
arkadian0 <adrianbukalski@gmail.com>
2017-11-22 23:32:19 +0100

    First Commit

---
+++ src/adis.java
@@ -0,0 +1,15 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class adis {
+
+	@Test
+	void test() {
+		adrian test = new adrian();
+		int output = test.adrianlicz(5);
+		assertEquals(25, output);
+	}
+
+}
---
+++ src/adrian.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+ public class adrian {
+    
+	int b;
+	static int adrianlicz(int a)
+	{
+		int wynik = 5;
+		
+		return wynik*a;
+	}
+	 
+	public static void main(String[] args) {
+		
+	}
+ }
+
